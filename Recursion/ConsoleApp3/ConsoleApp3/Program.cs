1fa0d21583bbfc53a36648e514427c10e972826a
leha404 <alekseychernavskiy@yandex.ru>
2019-02-22 23:42:42 +0500

    3 задачка решена!

--- Recursion/ConsoleApp3/ConsoleApp3/Program.cs
+++ Recursion/ConsoleApp3/ConsoleApp3/Program.cs
@@ -8,9 +8,38 @@ namespace ConsoleApp3
 {
     class Program
     {
+        static void RecursiveSubProgramPOW(int a_int, int x_int, int a_beg)
+        {
+            if (x_int == 1) Console.WriteLine(a_int);
+            else RecursiveSubProgramPOW(a_int * a_beg, x_int - 1, a_beg);
+        }
+
         static void Main(string[] args)
         {
+            int a_int, x_int;
+            bool a_bool, x_bool;
+            string s_input;
 
+            Console.Write("Введите число A: ");
+            s_input = Console.ReadLine();
+            a_bool = int.TryParse(s_input, out a_int);
+
+            Console.WriteLine();
+            Console.Write("Введите степень X: ");
+            s_input = Console.ReadLine();
+            x_bool = int.TryParse(s_input, out x_int);
+            Console.WriteLine();
+
+            if (a_bool && x_bool)
+            {
+                Console.Write("Число " + a_int + " в степени " + x_int + " = ");
+                RecursiveSubProgramPOW(a_int, x_int, a_int);
+            } else
+            {
+                Console.WriteLine("Вы ввели не число!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
